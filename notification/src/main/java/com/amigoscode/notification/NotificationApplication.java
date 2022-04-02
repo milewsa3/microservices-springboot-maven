de6bcad1575eafe615e8d9b66d045e135351fa0c
Adrian <a.milewski@onet.eu>
2022-04-02 09:03:52 +0200

    Send notifications to DB instead of logging them

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -3,8 +3,6 @@ package com.amigoscode.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
--- notification/src/main/java/com/amigoscode/notification/kafka/NotificationConsumer.java
+++ notification/src/main/java/com/amigoscode/notification/kafka/NotificationConsumer.java
@@ -1,15 +1,26 @@
 package com.amigoscode.notification.kafka;
 
 import com.amigoscode.clients.notification.NotificationRequest;
+import com.amigoscode.notification.NotificationService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class NotificationConsumer {
+
+    NotificationService notificationService;
 
     @KafkaListener(topics = "amigoscode", groupId = "groupId", containerFactory = "messageFactory")
     public void consumer(NotificationRequest request) {
-        System.out.println("Listener received: " + request + " ❤️");
+        log.info("Consumed {} from queue", request);
+        notificationService.send(request);
     }
 
 }
